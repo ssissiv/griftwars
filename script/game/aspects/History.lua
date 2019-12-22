9b59f582017d58aae7d0aee36d838cf63113d9f4
Unknown <ccc.costa@gmail.com>
2019-12-21 18:17:38 -0800

    Linked list history for fast removal

--- script/game/aspects/History.lua
+++ script/game/aspects/History.lua
@@ -1,19 +1,44 @@
 local History = class( "Aspect.History", Aspect )
 
+local MAX_SIZE = 1000
+
 function History:init()
 	self.items = {}
+	self.tail, self.head = nil, nil
+	self.count = 0
 end
 
 function History:Log( fmt, ... )
-	table.insert( self.items, { fmt, ... } )
+	local item = { fmt, ... }
 
-	-- TODO: Fix this shitty ring
-	while #self.items > 1024 * 32 do
-		table.remove( self.items, 1 )
+	if self.tail then
+		self.tail.next = item
+	end
+
+	self.tail = item
+	if self.head == nil then
+		self.head = item
+	end
+	self.count = self.count + 1
+
+	while self.count > MAX_SIZE do
+		self.head = self.head.next
+		self.count = self.count - 1
+	end
+end
+
+local function IterFn( state, i )
+	if i == nil then
+		state.iter = state.head
+	elseif state.iter then
+		state.iter = state.iter.next
+	end
+
+	if state.iter then
+		return (i or 0) + 1, state.iter
 	end
 end
 
 function History:Items()
-	return ipairs( self.items )
+	return IterFn, { head = self.head, iter = nil }
 end
-
