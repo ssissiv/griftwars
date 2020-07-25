fa28c9ca224d86ec387b18cc72cff7cdc4743d35
Unknown <ccc.costa@gmail.com>
2020-07-24 21:24:58 -0700

    Some rough cornered handling while fleeing

--- script/game/verbs/FleeFromCombat.lua
+++ script/game/verbs/FleeFromCombat.lua
@@ -5,7 +5,15 @@ function FleeFromCombat:GetDesc( viewer )
 end
 
 function FleeFromCombat:CalculateUtility( actor )
-	return UTILITY.EMERGENCY
+	if self:IsCornered() then
+		return UTILITY.COMBAT - 1
+	else
+		return UTILITY.EMERGENCY
+	end
+end
+
+function FleeFromCombat:IsCornered()
+	return (self.cornered or 0) > 1
 end
 
 function FleeFromCombat:CanInteract( actor )
@@ -19,33 +27,56 @@ function FleeFromCombat:CanInteract( actor )
 	return true
 end
 
+function FleeFromCombat:ClearCornered()
+	self.cornered = nil
+	print ("CLEARED CORNERED" )
+end
+
 function FleeFromCombat:Interact( actor )
 	-- Move away from the 2 closest targets.
-	local targets = actor.combat:GetTargetsByDistance()
-	local x0, y0 = actor:GetCoordinate()
-	local dir
-	if targets[2] then
-		local x1, y1 = targets[1]:GetCoordinate()
-		local x2, y2 = targets[2]:GetCoordinate()
-		local dx1, dy1 = x0 - x1, y0 - y1
-		local dx2, dy2 = x0 - x2, y0 - y2
-		local dx, dy = math.round( (dx1 + dx2) / 2 ), math.round( (dy1 + dy2) / 2 )
-		dir = VectorToDir( dx, dy )
+	while not self:IsCancelled() do
+		local targets = actor.combat:GetTargetsByDistance()
+		local x0, y0 = actor:GetCoordinate()
+		local target_dist = EntityDistance( actor, targets[1] )
+		local dir
+		if targets[2] then
+			local x1, y1 = targets[1]:GetCoordinate()
+			local x2, y2 = targets[2]:GetCoordinate()
+			local dx1, dy1 = x0 - x1, y0 - y1
+			local dx2, dy2 = x0 - x2, y0 - y2
+			local dx, dy = math.round( (dx1 + dx2) / 2 ), math.round( (dy1 + dy2) / 2 )
+			dir = VectorToDir( dx, dy )
+			target_dist = math.min( target_dist, EntityDistance( actor, targets[2] ))
 
-	elseif targets[1] then
-		local x1, y1 = targets[1]:GetCoordinate()
-		local dx, dy = x0 - x1, y0 - y1
-		dir = VectorToDir( dx, dy )
+		elseif targets[1] then
+			local x1, y1 = targets[1]:GetCoordinate()
+			local dx, dy = x0 - x1, y0 - y1
+			dir = VectorToDir( dx, dy )
+		end
+
+		local ok, reason = self:DoChildVerb( Verb.Walk( dir ))
+		if not ok then
+			-- Try an adjacent direction.
+			dir = table.arraypick( ADJACENT_DIR[ dir ] )
+			ok, reason = self:DoChildVerb( Verb.Walk( dir ))
+
+			if not ok then
+				if target_dist <= 1.5 then
+					self.cornered = (self.cornered or 0) + 1
+				end
+			else
+				self.cornered = nil
+			end
+		end
+		if not ok then
+			-- Wait a little.  Fleeing, so need to be responsive.
+			self:YieldForTime( WALK_TIME )
+			actor.behaviour:ScheduleNextTick( 0 )
+			break
+		end
 	end
 
-	local ok, reason = self:DoChildVerb( Verb.Walk( dir ))
-	if not ok then
-		-- Try an adjacent direction.
-		dir = table.arraypick( ADJACENT_DIR[ dir ] )
-		ok, reason = self:DoChildVerb( Verb.Walk( dir ))
-	end
-	if not ok then
-		-- Wait a little.  Fleeing, so need to be responsive.
-		self:YieldForTime( WALK_TIME )
+	if self.cornered then
+		self:ScheduleFunction( ONE_MINUTE, self.ClearCornered, self )
 	end
 end
