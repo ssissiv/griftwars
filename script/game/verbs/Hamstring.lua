6260b8c7e9557aeee58b9b369740dab5133dc65b
Unknown <ccc.costa@gmail.com>
2020-08-09 21:18:30 -0700

    Fix hamstring

--- script/game/verbs/Hamstring.lua
+++ script/game/verbs/Hamstring.lua
@@ -7,8 +7,8 @@ Hamstring.act_desc = "Hamstring"
 function Hamstring:Interact()
 	Hamstring._base.Interact( self )
 
-	if self.total_damage and not target:HasAspect( StatusEffect.Hobbled ) then
-		Msg:EchoTo( actor, "Your attack hobbles your victim's movement!" )
-		target:GainStatusEffect( StatusEffect.Hobbled, 1 )
+	if self.total_damage and not self.target:HasAspect( StatusEffect.Hobbled ) then
+		Msg:EchoTo( self.actor, "Your attack hobbles your victim's movement!" )
+		self.target:GainStatusEffect( StatusEffect.Hobbled, 1 )
 	end
 end
